ac32ee7dc4347cd54479e2033cc1faa7cf0479b6
daniilgrebnev <danilgrebneff@yandex.ru>
2025-03-02 20:59:28 +0300

    add

--- src/block-link/index.js
+++ src/block-link/index.js
@@ -4,9 +4,11 @@ import "./common.scss";
 import "./editor.scss";
 import "./style.scss";
 
+import save from "../block-text/save";
 import metadata from "./block.json";
 import Edit from "./edit";
 
 registerBlockType(metadata.name, {
 	edit: Edit,
+	save,
 });
--- src/block-link/save.js
+++ src/block-link/save.js
@@ -1,4 +1,6 @@
 import { useBlockProps } from "@wordpress/block-editor";
+import { RichText } from "@wordpress/block-editor/build/components";
+import { Icon } from "./assets/Icon";
 import "./common.scss";
 
 export default function save({ attributes }) {
@@ -10,6 +12,9 @@ export default function save({ attributes }) {
 			{...useBlockProps.save({
 				className: `block-link_content ${blockType}`,
 			})}
-		></a>
+		>
+			<Icon fill={blockType === "white" ? "#0f91d6" : "#fff"} width={18} />
+			<RichText.Content value={text} />
+		</a>
 	);
 }
