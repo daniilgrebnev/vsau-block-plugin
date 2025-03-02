b69eed4ffdba79813cda3d5d40a8d500cad826bd
daniilgrebnev <danilgrebneff@yandex.ru>
2025-03-02 21:11:52 +0300

    da

--- build/block-text/index.asset.php
+++ build/block-text/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'fe18ce50dc7b7884aa2d');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '24601b5d831550984f54');
