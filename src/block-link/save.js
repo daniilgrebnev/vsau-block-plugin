7f0343bc2ae189b3b13fc8c030f02dac881a31be
daniilgrebnev <danilgrebneff@yandex.ru>
2025-03-02 23:04:41 +0300

    ok1

--- src/block-link/save.js
+++ src/block-link/save.js
@@ -1,6 +1,7 @@
 import { useBlockProps } from "@wordpress/block-editor";
 import Icon from "./assets/Icon";
 import "./common.scss";
+import "./style.scss";
 
 export default function save({ attributes }) {
 	const { text, url, blockType } = attributes;
