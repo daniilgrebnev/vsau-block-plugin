b93e8834b8af78831d85043b9e628267480a2d86
daniilgrebnev <danilgrebneff@yandex.ru>
2026-03-17 00:00:10 +0300

    fd

--- build/block-switch/index.asset.php
+++ build/block-switch/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'bca85638e9fac066d1c6');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'a03b8b351ea39af2676b');
